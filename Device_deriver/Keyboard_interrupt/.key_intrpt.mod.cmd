ddf1b9603c0df66766c36809b8871908e2751c42
Podduturi Manasa <manasapodduturi@mirafra.com>
2024-12-11 09:41:18 +0530

    key_board interruppt

---
+++ Device_deriver/Keyboard_interrupt/.key_intrpt.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/km/Mirafra_LDD_BATCH/Linux_Driver/Device_deriver/Keyboard_interrupt/key_intrpt.mod := { echo  /home/km/Mirafra_LDD_BATCH/Linux_Driver/Device_deriver/Keyboard_interrupt/key_intrpt.o;  echo; } > /home/km/Mirafra_LDD_BATCH/Linux_Driver/Device_deriver/Keyboard_interrupt/key_intrpt.mod
